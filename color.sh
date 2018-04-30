34d5a7c3be5db09dc3cce5617018ade4b6131530
sylow_castle <fake_mail@example.com>
2018-04-30 21:58:07 +0900

    ヘルプ機能の追加

--- color.sh
+++ color.sh
@@ -1,5 +1,38 @@
 #!/usr/bin/bash
 
+usage=`cat << EOS
+<概要>
+標準入力の各行に対し、行頭の文字に合わせてその行の文字色を変更します。
+行頭の文字：文字色
++：赤色
+-：緑色
+その他：デフォルト色
+
+<オプション>
+ -h, --help：このヘルプを表示します。
+EOS
+`
+
+while getopts ":h-:" opt; do
+    case "$opt" in
+        -)
+        case "${OPTARG}" in
+            help)
+                #echo "helpがあ指定されました。"
+                echo "$usage"
+                exit 0
+            ;;
+        esac
+        ;;
+        h)
+            #-help指定のケースとまとめないのは数が少ないため。
+            #echo "hが指定されました。"
+            echo "$usage"
+            exit 0
+        ;;
+    esac
+done
+
 #標準入力に対して、行頭が+のときは赤く表示する
 #標準入力に対して、行頭が-のときは緑に表示する
 #どちらでもないときはデフォルト色で表示する
