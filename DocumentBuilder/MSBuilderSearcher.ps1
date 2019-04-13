aa55a3a0c5478143f45de9912281a5544b174443
sylow_castle <fake_mail@example.com>
2019-04-13 09:00:45 +0900

    [add] to reduce setting up time

---
+++ DocumentBuilder/MSBuilderSearcher.ps1
@@ -0,0 +1,48 @@
+﻿Write-Host 'コマンド"msbuild"が使えるか確認します…';
+if((Get-Command -CommandType All | Where-Object {$_.Name -like "msbuild.exe"} | Measure-Object).Count -ne 0) {
+    Write-Host "msbuildは使用可能です。";
+    exit;
+} else {
+    Write-Host "msbuildは使えません。探します。";
+}
+
+$require_verion = @{Major=4;Minor=0};
+
+$latest_msbuild = $null;
+Get-ChildItem -Recurse -File -Path 'C:\Windows\Microsoft.NET\Framework64' | 
+    Where-Object {$_.Name -like '*msbuild.exe'} |
+    ForEach-Object { Write-Output @{FullName=$_.FullName; Command=($_.FullName + " /version")} } |
+    ForEach-Object {
+        $_.Version = $(Invoke-Expression $_.Command | Where-Object {$_ -like "[0-9].[0-9]*"});
+        $_;
+    } |
+    ForEach-Object {  
+        $arr = $_.Version -split "\.";
+        $_.Major = [int]$arr[0];
+        $_.Minor = [int]$arr[1];
+        Write-Output $_;
+    } |
+    Where-Object { ($_.Major -ge $require_verion.Major) -and ($_.Minor -ge $require_verion.Minor)} | 
+    Sort-Object -Property Major, Minor |
+    Select-Object -First 1 | 
+    Set-Variable latest_msbuild;
+if($null -eq $latest_msbuild) {
+    Write-Host "対応しているMSbuildが見つかりませんでした。"
+    exit;
+} else {
+    Write-Host "対応しているMSbuildが見つかりました。"
+}
+
+Write-Host $latest_msbuild.FullName;
+$msbuild = Get-Item -LiteralPath $latest_msbuild.FullName;
+$msbuild.DirectoryName;
+[string]$response = Read-Host -Prompt ($msbuild.DirectoryName + " への Path（ユーザー環境変数）を通しますか？(Yes/No):");
+if(($response -like "yes") -or ($response -like "y")) {
+    $env_path = [System.Environment]::GetEnvironmentVariable('Path'); 
+    $msbuild = Get-Item -LiteralPath $latest_msbuild.FullName;
+    $env_path += [System.IO.Path]::PathSeparator + $msbuild.DirectoryName + [System.IO.Path]::DirectorySeparatorChar;
+    [System.Environment]::SetEnvironmentVariable('Path', $env_path, [System.EnvironmentVariableTarget]::User);
+    Write-Host "環境変数Pathを変更しました。msbuildを使用するにはPowershellを再起動してください。"
+} else {
+    Write-Host "環境変数Pathはそのままです。"
+}
--- DocumentBuilder/README.md
+++ DocumentBuilder/README.md
@@ -12,3 +12,39 @@ markdownで書類を一杯書くことになってきたけど、お客さんに
 
 指定したディレクトリ以下から.mdファイルを探してきてhtmlに変換するタスクを実行するよ。
 実際のビルドは何か他のツールに任せてあげてね。
+
+## 使い方
+
+### MSBuild版
+
+MSBuildはv4以上。なぜかというとプロパティ関数を使っているから。使える範囲広くしたいなぁ。
+
+1. pandocへのパスを通す。
+1. MSBuildへのパスを通す。面倒ならMSBuildSearcher.ps1とか実行する。場所は'C:\Windows\Microsoft.NET\Framework64\v4.0.30319'とかに入ってるはず。
+1. cmdかpowershellを起動して、このファイルのディレクトリにcdする。
+1. 'msbuild markdowns.proj -t:Artifact'って打つ。
+1. 'markdown'ディレクトリ以下のmdファイルがhtmlに変換されて出てくる。インクリメンタルビルドするように実装してあるので2回たたいても安心。
+
+### 制限
+
+* markdownたちがプロジェクトファイルのディレクトリ以下に入っているのを期待してる感じです。
+
+### 色々
+
+* 色変えたい
+    * markdown.css.htmlいじってね
+* markdownディレクトリじゃなきゃダメ？
+    * projファイルいじってね
+* pdfにできない？
+    * chromeへのパスを通してpdfartifactをターゲットにして実行してください。今のところインクリメンタルビルドじゃないので問題あり。
+* pandocって？
+    * mdをhtmlにしてくれるのに使うありがたいツール。Haskell製らしい：https://pandoc.org/
+* pandocないと使えない？
+    * 今のところは。ただここ変えたい。
+* MSBuildは標準で入ってるから使うのにpandoc使うの？それ標準じゃないよね？
+    * そうですね。
+
+## 参考
+
+example.htmlは以下のexample.htmlを使わせていただきました。
+https://github.com/simonlc/Markdown-CSS
